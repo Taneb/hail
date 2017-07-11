2200fd24867903f2cbe9eb887cb3c4b32c0115a4
Nathan van Doorn <nathan@myrtlesoftware.com>
2017-07-11 17:06:55 +0100

    Use TLS if possible

--- Hail/Hydra.hs
+++ Hail/Hydra.hs
@@ -4,9 +4,16 @@ module Hail.Hydra where
 import Control.Exception (handleJust)
 
 import Network.Wreq
+import Network.URI
+import Network.Connection (TLSSettings(TLSSettings))
 import Network.HTTP.Client (HttpException (InvalidUrlException))
+import Network.HTTP.Client.TLS (mkManagerSettings)
+import Network.TLS (clientSupported, defaultParamsClient, supportedCiphers)
+import Network.TLS.Extra (ciphersuite_default)
 import Data.Aeson.Lens
 import Control.Lens.Operators
+import Data.Default.Class (def)
+import Data.Maybe (fromJust)
 import Data.Text (unpack)
 
 -- | Get the out path of the latest build of a given hydra job.
@@ -26,6 +33,16 @@ getLatest uri m_creds =
   where
     opts = defaults & header "Accept" .~ ["application/json"]
                     & auth .~ m_creds
+                    & manager .~ Left (mkManagerSettings (TLSSettings clientParams) Nothing)
+
+    hostname = uriRegName . fromJust .
+               uriAuthority . fromJust $
+               parseURI uri
+
+    clientParams = (defaultParamsClient hostname "") {
+      clientSupported = def {supportedCiphers = ciphersuite_default}
+      }
+
     noOutErr =
       "Latest build of job " ++ uri
                              ++ " has no output named 'out'."
